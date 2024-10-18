7c8d8e6965172ebc3ef247e4e9f9041322f85a82
danFerLM <kiosamigp@gmail.com>
2024-10-18 15:36:18 -0500

    Update script.js

--- script.js
+++ script.js
@@ -16,3 +16,8 @@ const nav = document.querySelector('.nav-links');
 burger.addEventListener('click', () => {
     nav.classList.toggle('nav-active');
 });
+// Cambia el color del header al hacer scroll
+window.addEventListener('scroll', () => {
+    const header = document.querySelector('header');
+    header.classList.toggle('scrolled', window.scrollY > 0);
+});
