98aeeadad1e3ca9db7523891f56df5ccd1abc476
danFerLM <kiosamigp@gmail.com>
2024-12-15 14:10:19 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,3 @@
-// Efecto de scroll suave para la navegación
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-        e.preventDefault();
-
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
-            behavior: 'smooth'
-        });
-    });
-});
-
-// Menu Responsive
-const burger = document.querySelector('.burger');
-const nav = document.querySelector('.nav-links');
-
-burger.addEventListener('click', () => {
-    nav.classList.toggle('nav-active');
-});
-// Cambia el color del header al hacer scroll
-window.addEventListener('scroll', () => {
-    const header = document.querySelector('header');
-    header.classList.toggle('scrolled', window.scrollY > 0);
+document.addEventListener('DOMContentLoaded', () => {
+    alert('Gracias por visitar nuestra página. Tu ayuda es importante.');
 });
